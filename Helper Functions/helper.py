7ad322b0182c62e52e8546d001225cc8b0fea4e8
Abhimanyu Dasarwar <63962258+Abhimanyu9539@users.noreply.github.com>
2024-02-21 22:30:20 +0530

    Update helper.py

--- Helper Functions/helper.py
+++ Helper Functions/helper.py
@@ -4,6 +4,7 @@
 import tensorflow as tf
 
 # Create a function to import an image and resize it to be able to be used with our model
+# Updating the files
 def load_and_prep_image(filename, img_shape=224, scale=True):
   """
   Reads in an image from filename, turns it into a tensor and reshapes into
